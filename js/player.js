07bc2983db8a9b46f04c6067feef2904c0107eef
Eugene Koontz <ekoontz@hiro-tan.org>
2018-05-26 11:33:09 -0700

    initial commit

---
+++ js/player.js
@@ -0,0 +1,337 @@
+// -------- Script Globals ----------------------------------------------------
+
+// Status of player state machine. Starts at 'uninitialized'. 'fatal_error' is a
+// terminal end state. Once loaded OK then goes to 'ready'. If playing it will
+// be 'playing', if paused it will go back to 'ready' state. If the buffering
+// check detects it is buffering it will be 'buffering.'
+var g_player_state = 'uninitialized';
+var g_player_buffer_spinner_number = 0;
+var g_player_last_buffering_check = null;
+var g_player_last_playhead = null;
+var g_player_current_sound_id = null;
+var g_player_render_vu = false;
+
+var player_high_quality_enabled = true;
+var player_display_state;
+
+// -------- Initialization ----------------------------------------------------
+
+// soundManager.setup() needs to be called _before_ DOM Ready, so call now.
+soundManager.setup({
+//  debugFlash: true,
+//  debugMode: true,
+  flashVersion: 9,  // only flash 9 can play AAC or return peak values for VU
+  onready: onSoundManagerReady,
+  ontimeout: onSoundManagerTimeout,
+  preferFlash: false,
+  url: 'https://cdnjs.cloudflare.com/ajax/libs/soundmanager2/2.97a.20150601/swf/'
+});
+
+// -------- SoundManager State ------------------------------------------------
+
+function onSoundManagerReady() {
+  g_player_state = 'ready';
+  updatePlayStopButtonState();
+  var volume = getCurrentVolume();
+  soundManager.createSound({
+    autoPlay: false,
+    id: 'kfjc-mp3-high',
+    multiShot: false,
+    onfailure: onSoundFailure,
+    onfinish: onSoundFinish,
+    url: 'http://netcast6.kfjc.org/;stream',
+    volume: volume,
+  });
+  soundManager.createSound({
+    autoPlay: false,
+    id: 'kfjc-mp3-low',
+    duration: 0,
+    multiShot: false,
+    onfailure: onSoundFailure,
+    onfinish: onSoundFinish,
+    url: 'http://netcast4.kfjc.org:8974/;stream',
+    volume: volume,
+  });
+
+  // SM should actually make the determination to play the streams with Flash
+  // before we allow the user to see the VU meter button.
+  if (!soundManager.getSoundById('kfjc-mp3-low').isHTML5) {
+    $("#popup-show-vu-button-show-hide-container").attr('style', '');
+  }
+
+  // Steal focus back from SM elements.
+  $("#popup-play-pause-button").focus();
+}
+
+function loadArchive(playlistNum) {
+  deleteArchiveSounds();
+  var hours = kfjc.showList[playlistNum];
+  for (var i = 0; i < hours.length; i++) {
+    // TODO: extract out "archive" const
+    createArchiveSound("archive"+i, hours[i].url);
+  }
+  kfjc.nowPlayingMetadata = metadataFromArchive(playlistNum);
+  seekOverShow(kfjc.startPlaybackPosition);
+}
+
+function deleteArchiveSounds() {
+  // Create list of sounds to destroy so as not to modify
+  // soundManager.soundIDs during iteration.
+  var destroySounds = []
+  $.each(soundManager.soundIDs, function(index, soundId) {
+    if (soundId != undefined && soundId.startsWith("archive")) {
+      destroySounds.push(soundId);
+    }
+  });
+  $.each(destroySounds, function(index, soundId) {
+    soundManager.destroySound(soundId);
+  });
+}
+
+function createArchiveSound(id, url) {
+  soundManager.createSound({
+    autoPlay: false,
+    id: id,
+    multiShot: false,
+    onfailure: onSoundFailure,
+    onfinish: onSoundFinish,
+    url: url,
+    volume: getCurrentVolume(),
+  });
+}
+
+function seekOverShow(seekToMillis) {
+  var bounds = kfjc.selectedShowTime.bounds;
+  for (var i = 0; i < bounds.length; i++) {
+    if (seekToMillis <= bounds[i]) {
+      //seek to adjusted position
+      var thisSegmentStart = (i == 0) ? 0 : bounds[i-1];
+      var extraSeek = (i == 0) ? 0 : kfjc.showList[kfjc.selectedShowMetadata.id][i].padding_ms;
+      var localSeekTo = seekToMillis - thisSegmentStart + extraSeek;
+      soundManager.stopAll();
+      soundManager.unload(g_player_current_sound_id);
+      g_player_current_sound_id = "archive" + i;
+      soundManager.play(g_player_current_sound_id, {
+        position: localSeekTo,
+        onfinish: onSoundFinish
+      });
+      kfjc.nowPlayingArchiveIndex = i;
+      startSeekUiUpdate();
+      return;
+    }
+  }
+  updatePlayStopButtonState();
+}
+
+function startSeekUiUpdate() {
+  stopSeekUiUpdate();
+  seekUiUpdate();
+  kfjc.seekIntervalId = setInterval(seekUiUpdate, 1000);
+}
+
+function seekUiUpdate(opt_position) {
+  // TODO: boundary conditions
+  var position = opt_position != undefined ? opt_position : getPlayerPosition();
+  if (kfjc.archiveIsSliding) {
+    return;
+  }
+  $("#popup-seek-slider").val(position);
+  $("#popup-seek-time").text(positionFormat(position));
+}
+
+function positionFormat(p) {
+  // Use either ceil or floor, but not round, for best results updating the
+  // display every ~1sec.
+  var pos = Math.ceil(p / 1000) * 1000;
+  if (! isSelectedShortArchive()) {
+    pos -= 5 * 60 * 1000;         // TODO: don't hardcode this.
+  }
+  var isNegativeTime = (pos < 0);
+  var prepend = "";
+  if (isNegativeTime) {
+    pos = Math.abs(pos);
+    prepend = "-";
+  }
+  d = moment.utc(pos); 
+  return prepend + d.format("H:mm:ss");
+}
+
+function getPlayerPosition() {
+  if (kfjc.nowPlayingMetadata.id == 0) {
+    return 0;
+  }
+  var thisHourIndex = kfjc.nowPlayingArchiveIndex;
+  var segmentOffset = (thisHourIndex == 0) ? 0 : kfjc.selectedShowTime.bounds[thisHourIndex - 1];
+  var extra = (thisHourIndex == 0) ? 0 : kfjc.showList[kfjc.nowPlayingMetadata.id][thisHourIndex].padding_ms;
+  var playhead = soundManager.getSoundById(g_player_current_sound_id).position
+  return playhead + segmentOffset - extra;
+}
+
+function stopSeekUiUpdate() {
+  clearInterval(kfjc.seekIntervalId);
+}
+
+// Called when SM2 has given up trying to play a sound.
+function onSoundFailure() {
+  //TODO: actually display error.
+  displayError('Lost connection to KFJC audio server!');
+  g_player_state = 'ready';
+  updatePlayStopButtonState();
+}
+
+function onSoundFinish() {
+  var nextSoundIndex = kfjc.nowPlayingArchiveIndex + 1;
+  var nextSoundId = "archive" + nextSoundIndex;
+  if (soundManager.getSoundById(nextSoundId)) {
+    soundManager.stopAll();
+    soundManager.play(nextSoundId, {
+      position: isShortArchivePlaying() ? 0 : 10 * 60 * 1000, // TODO: don't hardcode
+      onfinish: onSoundFinish
+    });
+    kfjc.nowPlayingArchiveIndex++;
+    g_player_current_sound_id = nextSoundId;
+  } else {
+    stopPlaying();
+    updatePlayStopButtonState();
+  }
+}
+
+function onSoundManagerTimeout(status) {
+  g_player_state = 'fatal_error';
+  updatePlayStopButtonState();
+  displayError("Error loading player.");
+}
+
+function startPlaying() {
+  if (kfjc.selectedShowMetadata.id == 0) {
+    g_player_current_sound_id = player_high_quality_enabled ? 'kfjc-mp3-high' : 'kfjc-mp3-low';
+    kfjc.nowPlayingMetadata = kfjc.livestreamMetadata;
+  } else {
+    loadArchive(kfjc.selectedShowMetadata.id);
+  }
+  soundManager.setVolume(g_player_current_sound_id, getCurrentVolume());
+  soundManager.play(g_player_current_sound_id);
+  startReport30s();
+  g_player_state = 'playing';
+}
+
+function stopPlaying() {
+  kfjc.startPlaybackPosition = getPlayerPosition();
+  soundManager.stopAll();
+  soundManager.unload(g_player_current_sound_id);
+  g_player_current_sound_id = null;
+  g_player_state = 'ready';
+  stopSeekUiUpdate();
+  stopReport30s();
+  kfjc.nowPlayingMetadata = {};
+}
+
+function checkForBuffering() {
+  if (g_player_state == 'playing' || g_player_state == 'buffering') {
+    var now = new Date();
+    var playhead =
+        soundManager.getSoundById(g_player_current_sound_id).position;
+    if (g_player_last_buffering_check != null &&
+        g_player_last_playhead != null) {
+      var wall_clock_diff = now - g_player_last_buffering_check;
+      var audio_diff = playhead - g_player_last_playhead;
+      // Treat a difference of over 50% in playhead to wallclock as buffering.
+      if (audio_diff < wall_clock_diff * 0.5) {
+        g_player_state = 'buffering'
+        updatePlayStopButtonState();
+        g_player_buffer_spinner_number =
+            (g_player_buffer_spinner_number + 1) % 4;
+      } else {
+        g_player_state = 'playing';
+        g_player_buffer_spinner_number = 0;
+        updatePlayStopButtonState();
+      }
+    }
+    g_player_last_buffering_check = now;
+    g_player_last_playhead = playhead;
+  } else {
+    g_player_last_buffering_check = null;
+    g_player_last_playhead = null;
+  }
+}
+
+
+// -------- UI Manipulation ---------------------------------------------------
+
+function displayError(errorString) {
+  $("#popup-lower-controls").addClass('hidden');
+  $("#popup-error-message").removeClass('hidden');
+}
+
+function hideAllPlayStopButtons() {
+  $("#popup-play-button").hide();
+  $("#popup-stop-button").hide();
+  $("#popup-loading-button").hide();
+}
+
+function updatePlayStopButtonState() {
+  if (player_display_state == g_player_state) {
+    return;
+  }
+  player_display_state = g_player_state;
+  if (g_player_state == 'uninitialized') {
+    hideAllPlayStopButtons();
+    $("#popup-loading-button").show();
+  } else if (g_player_state == 'ready') {
+    hideAllPlayStopButtons();
+    $("#popup-play-button").show();
+  } else if (g_player_state == 'fatal_error') {
+    //TODO: disable play button
+  } else if (g_player_state == 'buffering') {
+    //TODO: consider showing buffering state
+  } else if (g_player_state == 'playing') {
+    hideAllPlayStopButtons();
+    $("#popup-stop-button").show();
+  }
+}
+
+// Returns the volume setting on the slider, or 1 if the Mute is pressed.
+function getCurrentVolume() {
+  return $("#popup-volume-slider").val();
+}
+
+function isPlaying() {
+  return g_player_state == 'playing';
+}
+
+// -------- UI Events ---------------------------------------------------------
+
+function onPlayButtonClicked() {
+  if (g_player_state == 'ready') {
+    startPlaying();
+  }
+  updatePlayStopButtonState();
+}
+
+function onStopButtonClicked() {
+  if (g_player_state == 'playing' || g_player_state == 'buffering') {
+    stopPlaying();
+  }
+  updatePlayStopButtonState();
+};
+
+function onVolumeChange() {
+  if (g_player_state == 'playing' || g_player_state == 'buffering') {
+    soundManager.setVolume(g_player_current_sound_id, getCurrentVolume());
+  }
+}
+
+function onSignalQualityClicked() {
+  player_high_quality_enabled = !player_high_quality_enabled;
+  if (player_high_quality_enabled) {
+    $("#popup-hq-button").text('HQ');
+  } else {
+    $("#popup-hq-button").text('LQ');
+  }
+
+  // Stop and re-start with the new quality level if we are currently playing.
+  if (g_player_state == 'playing' || g_player_state == 'buffering') {
+    stopPlaying();
+    startPlaying();
+  }
+}
---
+++ js/player_ui.js
@@ -0,0 +1,497 @@
+/*
+ *  Namespace for popup player UI variables.
+ */ 
+var kfjc = kfjc || {
+  archiveIsSliding: false,
+  showList: [],
+  nowPlayingArchiveIndex: 0,
+  selectedShowTime: 0,
+  startPlaybackPosition: 0,
+  livestreamMetadata: {
+    id: 0
+  },
+  nowPlayingMetadata: {},
+  selectedShowMetadata: {
+    id: 0
+  }
+};
+
+/*
+ * Convenience functions to determine if this show has 60- or 70-minute archives.
+ */
+function isSelectedShortArchive() {
+  if (kfjc.selectedShowMetadata.id == 0) {
+    return true;
+  }
+  return kfjc.selectedShowMetadata.id > 54940;
+}
+function isShortArchivePlaying() {
+  if (kfjc.nowPlayingMetadata.id == 0) {
+    return true;
+  }
+  return kfjc.nowPlayingMetadata.id > 54940;
+}
+
+/*
+ *  Main entry point once page is loaded.
+ */ 
+$(document).ready(function() {
+  loadArchives(parseHashNav);
+  initializeUIElements();
+  startMetadataUpdates();
+  startBufferingUpdates();
+});
+
+// -------- UI Backbone: Initialize, Invalidate and Resize--------------------------------------
+
+function initializeUIElements() {
+  startUpdateBackground();
+
+  // Play-Pause button starts out disabled until SoundManager reports ready.
+  $("#nav-playlist").click(togglePlaylist);
+  $("#popup-play-button").click(onPlayButtonClicked);
+  $("#popup-stop-button").click(onStopButtonClicked);
+  $("#popup-hq-button").click(onSignalQualityClicked);
+  $("#popup-volume-slider").on("input change", onVolumeChange);
+  $("#popup-volume").hover(showVolume, hideVolume);
+  $("#popup-volume-button").click(showVolume);
+  $("#popup-play-archive-button").click(playThisInstead);
+  $("#popup-seek-slider").on("change", function() {
+    if (isPlaying()) {
+      seekOverShow($("#popup-seek-slider").val());
+    } else {
+      kfjc.startPlaybackPosition = $("#popup-seek-slider").val();
+    }
+  });
+  $("#popup-now-playing-overlay").click(function() {
+    loadShow(kfjc.nowPlayingMetadata.id);
+  });
+  $("#popup-seek-slider").on('mousedown touchstart', function() {
+    kfjc.archiveIsSliding = true;
+  });
+  $("#popup-seek-slider").on('input', function() {
+    var val = $("#popup-seek-slider").val();
+    $("#popup-seek-time").text(positionFormat(val));
+  });
+  $("#popup-seek-slider").on('mouseup touchend', function() {
+    kfjc.archiveIsSliding = false;
+  });
+  $("#burger-bar").on("open.offcanvas", function() {
+    loadArchives();
+  });
+  $(window).resize(function() {
+    resize();
+  });
+
+  window.onhashchange = parseHashNav;
+  updatePlayStopButtonState();
+}
+
+function invalidateUi() {
+  $(".now-playing-dj").text(kfjc.nowPlayingMetadata.airName);
+  $(".current-live-dj").text(kfjc.livestreamMetadata.airName);
+
+  var isSomethingElseDisplayed = false;
+
+  if (kfjc.selectedShowMetadata.missingAudio) {
+    $("#popup-lower-controls").hide();
+    if (!kfjc.selectedShowMetadata.airName) {
+      updatePlaylist();
+      showPlaylist();
+    }
+    resize();
+    return;
+  } else {
+    $("#popup-lower-controls").show();
+  }
+
+  if (isPlaying() && kfjc.selectedShowMetadata.id != kfjc.nowPlayingMetadata.id) {
+    isSomethingElseDisplayed = true;
+    $("#popup-now-playing-overlay").show();
+    $("#popup-play-archive").show();
+    $("#popup-controls").hide();
+  } else {
+    $("#popup-now-playing-overlay").hide();
+    $("#popup-play-archive").hide();
+    $("#popup-seek").show();
+    $("#popup-controls").show();
+  }
+
+  if (kfjc.selectedShowMetadata.id == 0) {
+    $("#popup-now-playing").html(kfjc.livestreamMetadata.nowPlayingText);
+    $("#popup-selected-dj").text(kfjc.livestreamMetadata.airName);
+    $(".archive-controls").hide();
+    $(".live-controls").show();
+  } else {
+    $("#popup-now-playing").text(kfjc.selectedShowMetadata.nowPlayingText);
+    $("#popup-selected-dj").text(kfjc.selectedShowMetadata.airName);
+    $("#popup-seek-slider").attr("max", kfjc.selectedShowTime.total);
+    $(".live-controls").hide();
+    if (!isSomethingElseDisplayed) {
+      $(".archive-controls").show();
+    } else {
+      $(".archive-controls").hide();
+    }
+  }
+
+  resize();
+  updatePlaylist();
+}
+
+function resize() {
+  var a = $("#popup-lower-controls").is(":visible") ? $("#popup-lower-controls").height() : 0;
+  var b = parseInt($("#popup-upper-margin").css("margin-top"));
+  var c = parseInt($("#popup-upper-margin").css("margin-bottom"));
+  var d = $("#popup-upper-info").outerHeight();
+  var e = $("#popup-now-playing-overlay").is(":visible") ? $("#popup-now-playing-overlay").outerHeight() : 0;
+
+  var playlistHeight = $(window).height() - a - b - c - d - e - 2;
+  $("#playlist").height(playlistHeight);
+
+  var aa = $("#burger-header").outerHeight();
+  var burgerBodyHeight = $(window).height() - aa - 48;
+  $("#burger-body").height(burgerBodyHeight);
+}
+
+function startMetadataUpdates() {
+  updateMetadata();
+  setInterval(updateMetadata, 30000);
+}
+
+function startBufferingUpdates() {
+  setInterval(checkForBuffering, 500);
+}
+
+// -------- AJAX Fetchers and callbacks --------------------------------------------------
+
+function loadArchives(opt_callback) {
+  $.getJSON(kfjc_base_url + "/api/archives.php", function(res) {
+    kfjc.showList = buildShowList(res);
+    var showListTemplateData = buildShowListTemplateData(kfjc.showList);
+    renderShowList(showListTemplateData);
+    if (opt_callback) {
+      opt_callback();
+    }
+  });
+}
+
+function updateMetadata() {
+  $.ajax({
+    url: kfjc_base_url + '/api/playlists',
+    dataType: 'json',
+    success: setMetadata,
+    cache: false,
+    error: updateMetadataFailed
+  });
+}
+
+function setMetadata(data, textStatus, jqXHR) {
+  var liveShow = data.show_info;
+  var livePlaylist = data.playlist;
+  var liveTrack = livePlaylist[livePlaylist.length - 1];
+
+  var nowPlayingText = "";
+    if (liveTrack.artist && liveTrack.track_title) {
+      nowPlayingText = liveTrack.artist + " &ndash; " + liveTrack.track_title;
+    } else if (liveTrack.artist) {
+      nowPlayingText = liveTrack.artist;
+    } else if (liveTrack.track_title) {
+      nowPlayingText = liveTrack.track_title;
+    }
+  kfjc.livestreamMetadata = {
+    id: 0,
+    airName: (liveShow && liveShow.air_name) ? liveShow.air_name : 'George Foothill',
+    nowPlayingText: nowPlayingText
+  };
+  invalidateUi();
+}
+
+function updateMetadataFailed() {
+  $("#popup-now-playing-track").text('');
+  $("#popup-now-playing-artist").text('');
+  $("#popup-current-dj").text('George Foothill');
+  invalidateUi();
+}
+
+function startUpdateBackground() {
+  $.ajax({
+    url: kfjc_base_url + '/api/resources.json',
+    dataType: 'json',
+    success: onUpdateBackground,
+    cache: false
+  });
+  window.setTimeout(startUpdateBackground, getMillisUntilNextHour());
+}
+
+function onUpdateBackground(data, textStatus, jqXHR) {
+  if (data.drawables.backgrounds) {
+    var backgrounds = data.drawables.backgrounds;
+    var date = new Date();
+    var hour = date.getHours();
+    $("body").css('background-image', 'url(' + backgrounds[hour] + ')');
+  }
+}
+
+function updatePlaylist() {
+  var playlistId = kfjc.selectedShowMetadata.id;
+
+  $.ajax({
+    url: kfjc_base_url + '/api/playlists?i=' + playlistId,
+    dataType: 'json',
+    success: updatePlaylistCallback,
+    cache: false,
+    error: updateMetadataFailed
+  });
+}
+
+function updatePlaylistCallback(data, textStatus, jqXHR) {
+  $.each(data.playlist, function(_, track) {
+    if (track.time_played) {
+      track.time_format = playlistTimeFormat(moment(track.time_played * 1000));
+    } else {
+      track.time_format = "";
+    }
+  });
+  if (!data.show_info) {
+    data.show_info = {
+      air_name: "George Foothill",
+      start_time_format: ""
+    }
+  }
+  if (data.show_info.start_time) {
+    data.show_info.start_time_format = showTimeFormat(roundUpHour(data.show_info.start_time));
+  }
+  if (kfjc.selectedShowMetadata.missingAudio) {
+    // It's also missing show data which should be populated from playlist.
+    kfjc.selectedShowMetadata.airName = data.show_info.air_name;
+    kfjc.selectedShowMetadata.nowPlayingText = data.show_info.start_time_format;
+    $("#popup-now-playing").text(kfjc.selectedShowMetadata.nowPlayingText);
+    $("#popup-selected-dj").text(kfjc.selectedShowMetadata.airName);
+  }
+
+  var playlistTemplate = doT.template($('#modalPlaylistTemplate').text());
+  $('#playlist').html(playlistTemplate(data));
+}
+
+function startReport30s() {
+  kfjc.reportIntervalId = setInterval(report30s, 30000);
+}
+
+function stopReport30s() {
+  clearInterval(kfjc.reportIntervalId);
+}
+
+function report30s() {
+  if (!isPlaying() || kfjc.nowPlayingMetadata.id == undefined) {
+    return;
+  }
+  var data = {
+    "type": "PLAYER_30S",
+    "msg": "showId:" + kfjc.nowPlayingMetadata.id
+  }
+  $.post(kfjc_base_url + "/api/metrics/report.php", data);
+}
+
+// -------- Unsorted stuff --------------------------------------------------
+/*
+ *  Consumes JSON archive list from kfjc.org/api/archives.php
+ *  Produces map of { playlist_num: [Array of hourly chunks] }
+ */
+function buildShowList(archives) {
+  var shows = {}
+
+  $.each(archives, function(i, archive) {
+    if (! shows[archive.playlist_num]) {
+      shows[archive.playlist_num] = [];
+    }
+    shows[archive.playlist_num].unshift(archive)
+  });
+
+  return shows;
+}
+
+/*
+ *  Consumes map of { playlist_num: [Array of hourly chunks] }
+ *  Produces { shows: [Array of show template data]}
+ */
+function buildShowListTemplateData(showList) {
+  var showTemplateData = {
+    shows: []
+  }
+
+  $.each(showList, function(i, show) {
+    // Round to the top of the hour by adding 30mins and taking the start of that hour.
+
+    var startTime = roundUpHour(show[0].start_time);
+    var timeDayFormat = startTime.tz('America/Los_Angeles').format("hA dddd");
+    var dateFormat = startTime.tz('America/Los_Angeles').format("Do MMMM YYYY");
+
+    if (show[0].playlist_num != 0) {
+      showTemplateData.shows.push({
+        djName: show[0].air_name,
+        timeDayFormat: timeDayFormat,
+        dateFormat: dateFormat,
+        playlist_num: show[0].playlist_num
+      });
+    }
+  });
+
+  showTemplateData.shows.reverse();
+  return showTemplateData;
+}
+
+/*
+ *  Renders the list of shows from the 2-week archive.
+ */
+function renderShowList(showTemplateData) {
+  var showTemplate = doT.template($('#showTemplate').text());
+
+  // Render the books using the template
+  $("#archive-list").empty();
+  $("#archive-list").append(showTemplate(showTemplateData));
+}
+
+/*
+ * Loads UI to reflect selected show.
+ * playlistNum = 0 refers to the live stream.
+ */
+function loadShow(playlistNum, positionMillis) {
+  if (playlistNum != 0) {
+    if (playlistNum in kfjc.showList) {
+      kfjc.selectedShowTime = calculateShowTime(playlistNum);
+      kfjc.selectedShowMetadata = metadataFromArchive(playlistNum);
+    } else {
+      kfjc.selectedShowMetadata = {id: playlistNum, missingAudio: true}
+    }
+    var hours = kfjc.showList[playlistNum];
+    kfjc.startPlaybackPosition = positionMillis ? positionMillis : hours[0].padding_ms;
+  } else {
+    kfjc.selectedShowMetadata = kfjc.livestreamMetadata;
+  }
+  invalidateUi();
+}
+
+function metadataFromArchive(playlistNum) {
+  var hours = kfjc.showList[playlistNum];
+  var startTime = roundUpHour(hours[0].start_time);
+  var timeDayFormat = startTime.tz('America/Los_Angeles').format("hA, dddd Do MMMM YYYY");
+  return {
+    id: playlistNum,
+    airName: hours[0].air_name ? hours[0].air_name : 'George Foothill',
+    nowPlayingText: timeDayFormat
+  };
+}
+
+function togglePlaylist() {
+  if ( $('#upper-slide-container').is(":visible")) {
+    hidePlaylist();
+  } else {
+    showPlaylist();
+  }
+}
+
+function showPlaylist() {
+  $("#nav-playlist-icon").addClass("flip-x");
+  if (! $('#upper-slide-container').is(":visible")) {
+    $('#upper-slide-container').show(0, function() {
+      $('#playlist').slideDown('fast');
+    });
+  }
+}
+
+function hidePlaylist() {
+  $("#nav-playlist-icon").removeClass("flip-x");
+  if ( $('#upper-slide-container').is(":visible")) {
+    $('#playlist').slideUp('fast', function() {
+      $('#upper-slide-container').hide();
+    });
+  }
+}
+
+function playThisInstead() {
+  onStopButtonClicked();
+  kfjc.startPlaybackPosition = isSelectedShortArchive() ? 0 : 300000;
+  onPlayButtonClicked();
+  invalidateUi();
+}
+
+function showVolume() {
+  if ($("#popup-volume-slider").is(":visible")) {
+    return;
+  }
+  $("#popup-volume-slider").show();
+  $("#popup-volume-slider").animation("slideInLeft");
+
+}
+
+function hideVolume() {
+  if ($("#popup-volume-slider").is(":visible") == undefined) {
+    return;
+  }
+  $("#popup-volume-slider").show();
+  $("#popup-volume-slider").animation("slideOutLeft");
+}
+
+function calculateShowTime(playlistNum) {
+  var shows = kfjc.showList[playlistNum];
+  var totalTime = 0;
+  var bounds = [];
+
+  for (var i = 0; i < shows.length; i++) {
+    var hourPaddingTimeMillis = shows[i].padding_ms;
+    var segmentTime = shows[i].duration_ms;
+    totalTime += segmentTime - 2 * hourPaddingTimeMillis;
+    // Bounds
+    bounds[i] = totalTime + hourPaddingTimeMillis;
+    if (i == shows.length - 1) {
+        bounds[i] += hourPaddingTimeMillis;
+    }
+  }
+  totalTime += 2 * shows[0].padding_ms;
+  return {total: totalTime, bounds: bounds};
+}
+
+function parseHashNav() {
+  var hashNav = $(location).attr('hash').replace("#", "").split(".");
+  var showId = hashNav[0];
+  var position = hashNav[1];
+  if (showId) {
+    if (isSelectedShortArchive()) {
+      positionMillis = position ? position * 1000 : 0;
+    } else {
+      positionMillis = position ? position * 1000 : 300000; // TODO: don't hard code 300000
+    }
+    loadShow(showId, positionMillis);
+    seekUiUpdate(positionMillis);
+  }
+}
+
+// -------- Utilities --------------------------------------------------------------------
+function showTimeFormat(startMoment) {
+  // 3PM, Saturday 19th August 2017
+  return startMoment.tz('America/Los_Angeles').format("hA, dddd D MMMM YYYY");
+}
+
+function playlistTimeFormat(startMoment) {
+  // 4:20pm
+  return startMoment.tz('America/Los_Angeles').format("h:mma");
+}
+
+function getMillisUntilNextHour() {
+  return 3600000 - (new Date().getTime() % 3600000);
+}
+
+function roundUpHour(time) {
+  return moment(time * 1000)
+    .add(30, "minutes").startOf("hour");
+}
+
+/*
+ * Polyfill for IE.
+ */
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
